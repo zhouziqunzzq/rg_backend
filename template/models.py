3e35ed448c445f259706c53fd27a0d16d11fcf1f
Bittersweet <zhouziqun@cool2645.com>
2018-08-21 15:42:13 +0800

    Add rhyme toggle

--- template/models.py
+++ template/models.py
@@ -9,10 +9,11 @@ class Template(models.Model):
 
 
 class Verse(models.Model):
-    sentence_count = models.IntegerField()
-    word_count = models.IntegerField()
-    rhyme_mode = models.IntegerField()  # 1 - 单押, 2 - 双押
-    rhyme_style_id = models.IntegerField()  # 0 - 排韵, 1 - 交韵, 2 - 隔行押, 3 - 抱韵
+    sentence_count = models.IntegerField(default=4)
+    rhyme_toggle = models.BooleanField(default=True)
+    word_count = models.IntegerField(default=0)
+    rhyme_mode = models.IntegerField(default=1)  # 1 - 单押, 2 - 双押
+    rhyme_style_id = models.IntegerField(default=0)  # 0 - 排韵, 1 - 交韵, 2 - 隔行押, 3 - 抱韵
     template = models.ForeignKey(
         Template,
         on_delete=models.CASCADE
@@ -20,7 +21,6 @@ class Verse(models.Model):
 
     def __str__(self):
         return self.template.name + " - Verse(" + str(self.id) + ")"
-
 
 # class Sentence(models.Model):
 #     wordCount = models.IntegerField()
